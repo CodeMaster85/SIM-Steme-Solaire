738bd54244f2a8a43abc512a4a7407b2788dc54b
Yohan Chuet <202284927@cegepsherbrooke.qc.ca>
2024-03-15 09:08:09 -0400

    test commit project EU_5

---
+++ SIM_Steme_Solaire/Source/SIM_Steme_Solaire/SIM_Steme_Solaire.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SIM_Steme_Solaire.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SIM_Steme_Solaire, "SIM_Steme_Solaire" );
